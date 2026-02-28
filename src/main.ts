42bf5f73771376dcf3dc29193069993c336337ef
ramengo <angelocantalp@gmail.com>
2026-02-28 17:51:37 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,6 @@ import "./plugins";
 import "./registerServiceWorker";
 
 import App from "./App.vue";
-
 // Enable compatibilty mode for array updates for @duet3d/objectmodel library
 (window as any)._duetModelSetArray = (array: object, index: string | number, value: any) => Vue.set(array, index, value);
 
