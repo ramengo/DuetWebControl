e8cc7bf7922aac56fef24d4ae6fcaca12acb8d8b
ramengo <angelocantalp@gmail.com>
2026-07-25 17:34:31 +0200

    start no animation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -95,7 +95,7 @@ export const Menu = Vue.observable<Record<string, MenuCategory>>({
 			{
 				icon: "mdi-list-status",
 				caption: "menu.control.status",
-				condition: () => Vue.prototype.$vuetify && Vue.prototype.$vuetify.breakpoint.smAndDown,
+				condition: false,
 				path: "/Status",
 				component: Status
 			},
